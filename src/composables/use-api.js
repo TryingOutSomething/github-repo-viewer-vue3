78071fec32012f6ab983c4b5dd67293e95804499
TryingOutSomething <jan.owyeong123@gmail.com>
2020-08-07 15:07:17 +0800

    Added use-api function

--- src/composables/use-api.js
+++ src/composables/use-api.js
@@ -1,9 +1,34 @@
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs } from "vue";
+import api from "@/services/api";
 
-export default function () {
+export default function() {
   const state = reactive({
     response: [],
+    totalCount: 0,
     error: null,
     loading: false
-  })
+  });
+
+  const fetchRepositories = async args => {
+    state.loading = true;
+
+    try {
+      const response = await api.getRepositories(args);
+      console.log(response.data);
+
+      state.response = response.data.items;
+      state.totalCount = response.data.total_count;
+    } catch (err) {
+      console.log(err);
+      console.log(err.response);
+      state.error = err;
+    } finally {
+      state.loading = false;
+    }
+  };
+
+  return {
+    ...toRefs(state),
+    fetchRepositories
+  };
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = 'https://api.github.com/search'
+const BASE_URL = "https://api.github.com/search";
 
 const apiClient = axios.create({
   baseURL: BASE_URL
-})
+});
 
-const DEFAULT_ITEMS_PER_PAGE = '10'
-const DEFAULT_SORT_ORDER = 'desc'
-const DEFAULT_SORT_BY = 'stars'
-const DEFAULT_CURRENT_PAGE = '1'
+const DEFAULT_ITEMS_PER_PAGE = "10";
+const DEFAULT_SORT_ORDER = "desc";
+const DEFAULT_SORT_BY = "stars";
+const DEFAULT_CURRENT_PAGE = "1";
 
-function queryStringBuilder ({
+function queryStringBuilder({
   page = DEFAULT_CURRENT_PAGE,
   itemsPerPage = DEFAULT_ITEMS_PER_PAGE,
   sortOrder = DEFAULT_SORT_ORDER
 } = {}) {
-  const customQuery = 'language:vue'
+  const customQuery = "language:vue";
 
-  return `q=${customQuery}&sort=${DEFAULT_SORT_BY}&order=${sortOrder}&per_page=${itemsPerPage}&page=${page}`
+  return `q=${customQuery}&sort=${DEFAULT_SORT_BY}&order=${sortOrder}&per_page=${itemsPerPage}&page=${page}`;
 }
 
 export default {
-  getRepositories (args) {
-    const queryString = queryStringBuilder(args)
+  getRepositories(args) {
+    const queryString = queryStringBuilder(args);
 
-    return apiClient.get(`${BASE_URL}/repositories?${queryString}`)
+    return apiClient.get(`${BASE_URL}/repositories?${queryString}`);
   }
-}
+};
